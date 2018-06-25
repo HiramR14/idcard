ecf2e8999ee2946d679f3e22fc21a8b576092b18
Hiram Rivera <hiramr14@yahoo.com>
2018-06-25 12:03:10 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function generateID() {
+    let userFirstName = document.getElementById("firstName").value;
+    console.log(userFirstName);
+    document.getElementById("idCard").innerHTML = userFirstName;
+
+
+
+
+
+
+    let userLastName = document.getElementById("lastName").value;
+    console.log(userLastName);
+    document.getElementById("idCard").innerHTML = userLastName;
+}
+
+    // Last Name
+
+    // Age
+
+    // Phone Number
+
+    // Address
